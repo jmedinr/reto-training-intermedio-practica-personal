c3b4e692bc60ed70f63c22eb9488376c5e803ea5
jmedinr <juandiego619@gmail.com>
2022-06-09 11:36:07 -0500

    ADD --- se agrega componente para salir

---
+++ web/src/app/navbar-out/navbar-out.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavbarOutComponent } from './navbar-out.component';
+
+describe('NavbarOutComponent', () => {
+  let component: NavbarOutComponent;
+  let fixture: ComponentFixture<NavbarOutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NavbarOutComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavbarOutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
